b1d14f8908db18161640103820e65f21a619a30d
Mauro Elias <mauroelias1986@gmail.com>
2020-09-29 22:11:33 -0300

    1.3.1

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,6 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "1000px";
+    document.getElementById("mySidebar").style.width = "800px";
     document.getElementById("main").style.marginLeft = "1000px";
   }
   
