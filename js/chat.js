ea6373eb1e6a2b607b4eb5ad8324aa1d3198d45f
Mauro Elias <mauroelias1986@gmail.com>
2020-10-01 00:59:53 -0300

    1.3.1

--- js/chat.js
+++ js/chat.js
@@ -1,19 +1,19 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "800px";
-    document.getElementById("main").style.marginLeft = "800px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+  document.getElementById("mySidebar").style.width = "800px";
+  document.getElementById("main").style.marginLeft = "800px";
+}
 
-  // CHAT
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
 
-  $(document).ready(function(){
-    $('#action_menu_btn').click(function(){
-        $('.action_menu').toggle();
-    });
-        });
+// CHAT
+
+$(document).ready(function () {
+  $('#action_menu_btn').click(function () {
+    $('.action_menu').toggle();
+  });
+});
